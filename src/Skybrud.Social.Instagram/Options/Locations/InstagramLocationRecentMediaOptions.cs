c7e81742864e205357981d5085c29f80f8fea9cd
Anders Bjerner <abjerner@skybrud.dk>
2015-12-31 20:10:58 +0100

    Initial commit (code copied from the main Skybrud.Social repository)

---
+++ src/Skybrud.Social.Instagram/Options/Locations/InstagramLocationRecentMediaOptions.cs
@@ -0,0 +1,10 @@
+﻿using Skybrud.Social.Instagram.Options.Common;
+
+namespace Skybrud.Social.Instagram.Options.Locations {
+    
+    /// <summary>
+    /// Class representing the options for getting recent media of a location.
+    /// </summary>
+    public class InstagramLocationRecentMediaOptions : InstagramRecentMediaOptions { }
+
+}
---
+++ src/Skybrud.Social.Instagram/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Skybrud.Social.Instagram")]
+[assembly: AssemblyDescription("Authentication and service implementation for the Instagram API.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Skybrud.Social.Instagram")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37b5b960-dc9e-4fdc-8dfb-f87a114b54f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1.0")]
+[assembly: AssemblyMetadata("StartDate", "2012-07-30")]
