f0096396b215688c580489d43f14a8eda2376c6f
Anders Bjerner <abjerner@skybrud.dk>
2020-02-14 22:03:20 +0100

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,2 @@
 @echo off
-cd src/Skybrud.Social.Instagram
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget /p:RunBuildTargets=false
-cd ../../
-grunt
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.Social.Instagram /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
